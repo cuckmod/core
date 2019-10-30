8c464241464e476026868bcb875b8b2451ac6728
Danylo Prykhodko <drprykhodko@gmail.com>
2019-10-30 18:46:02 +0200

    Added entity support.

---
+++ entity.go
@@ -0,0 +1,56 @@
+package core
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"reflect"
+	"sync"
+)
+
+type (
+	Entity interface {
+		Get(tx *sql.Tx) error
+
+		// SQL
+		Scan(interface{}) error
+		Value() (driver.Value, error)
+	}
+)
+
+func GetAll(tx *sql.Tx, e Entity) (err error) {
+	errChan := make(chan error)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		innerGet(&wg, errChan, tx, e)
+	}()
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+	for err := range errChan {
+		if err != nil {
+			return err
+		}
+	}
+	return
+}
+
+func innerGet(wg *sync.WaitGroup, errChan chan error, tx *sql.Tx, e Entity) {
+	if err := e.Get(tx); err != nil {
+		errChan <- err
+		return
+	}
+	modelValue := reflect.ValueOf(e).Elem()
+	for i := 0; i < modelValue.NumField(); i++ {
+		field := modelValue.Field(i)
+		if field.Type().Implements(reflect.ValueOf(new(Entity)).Type().Elem()) {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				innerGet(wg, errChan, tx, field.Interface().(Entity))
+			}()
+		}
+	}
+}
---
+++ entity_test.go
@@ -0,0 +1,69 @@
+package core
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"testing"
+)
+
+type FooEntity struct {
+	Id  int
+	Bar *BarEntity
+}
+
+func (f *FooEntity) Get(*sql.Tx) error {
+	f.Bar = &BarEntity{
+		Id: f.Id + 1,
+	}
+	return nil
+}
+
+func (f *FooEntity) Scan(interface{}) error {
+	return nil
+}
+
+func (f *FooEntity) Value() (driver.Value, error) {
+	return nil, nil
+}
+
+type BarEntity struct {
+	Id  int
+	Baz BazEntity
+}
+
+func (b *BarEntity) Get(*sql.Tx) error {
+	b.Baz = BazEntity{
+		Id: b.Id + 1,
+	}
+	return nil
+}
+
+func (b *BarEntity) Scan(interface{}) error {
+	return nil
+}
+
+func (b *BarEntity) Value() (driver.Value, error) {
+	return nil, nil
+}
+
+type BazEntity struct {
+	Id int
+}
+
+func TestGetAll(t *testing.T) {
+	a := FooEntity{
+		Id: 1,
+	}
+	err := GetAll(nil, &a)
+	if err != nil {
+		t.Error(err)
+	}
+	if a.Bar.Id != a.Id+1 {
+		t.Log(a.Bar.Id)
+		t.Fail()
+	}
+	if a.Bar.Baz.Id != a.Bar.Id+1 {
+		t.Log(a.Bar.Baz.Id)
+		t.Fail()
+	}
+}
